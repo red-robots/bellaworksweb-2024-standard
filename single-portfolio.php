df672f6f80a226510f8b705b0f5fdf8649bed203
Lisa <lisaqdebona@gmail.com>
2024-07-28 01:43:17 -0400

    Made changes on single portfolio page

--- single-portfolio.php
+++ single-portfolio.php
@@ -55,99 +55,114 @@ get_header(); ?>
 			        	</div> -->
 		        	</div>
 		        	
-		        	
+		        	<?php if ($intro_text) { ?>
 		        	<div class="desc" style="color: <?php echo $text_color; ?>">
 		        		<?php echo $intro_text; ?>
 		        	</div>
+              <?php } ?>
 		        </div>
 		        <?php
 		        // Case: Download layout.
 		        elseif( get_row_layout() == 'hero' ): 
-		            $hero_image = get_sub_field('hero_image');
-				?>
-		        <div class="hero">
-		        	<img src="<?php echo $hero_image['url']; ?>">
-		        </div>
-
-		        <?php
+		          if( $hero_image = get_sub_field('hero_image') ) { ?>
+  		        <div class="hero">
+  		        	<img src="<?php echo $hero_image['url']; ?>">
+  		        </div>
+              <?php } ?>
+		        
+            <?php
 		        // Case: Download layout.
 		        elseif( get_row_layout() == 'second_section' ): 
-		            $color_logo = get_sub_field('color_logo');
-		            $small_blurg = get_sub_field('small_blurg');
-		        ?>
-		        <div class="blurbs">
-		        	<div class="color-logo">
-		        		<div class="logo">
-		        			<img src="<?php echo $color_logo['url']; ?>">
-		        		</div>
-		        	</div>
-		        	<div class="blurb">
-		        		<?php echo $small_blurg; ?>
-		        	</div>
-		        </div>
+	            $color_logo = get_sub_field('color_logo');
+	            $small_blurg = get_sub_field('small_blurg');
+              if( $color_logo || $small_blurg ) { ?>
+  		        <div class="blurbs">
+                <?php if ($color_logo) { ?>
+  		        	<div class="color-logo">
+  		        		<div class="logo">
+  		        			<img src="<?php echo $color_logo['url']; ?>">
+  		        		</div>
+  		        	</div>
+                <?php } ?>
+
+                <?php if ($small_blurg) { ?>
+  		        	<div class="blurb">
+  		        		<?php echo $small_blurg; ?>
+  		        	</div>
+                <?php } ?>
+  		        </div>
+              <?php } ?>
 		        <?php 
 		        // Case: Download layout.
 		        elseif( get_row_layout() == 'third_section' ): 
-		            $detail_photo = get_sub_field('detail_photo');
-		            $full_site_photo = get_sub_field('full_site_photo');
-		            $key_features = get_sub_field('key_features');
-		        ?>
-		        <div class="details">
-		        	<div class="left">
-                <?php if ($detail_photo) { ?>
+	            $detail_photo = get_sub_field('detail_photo');
+              $detail_photo2 = get_sub_field('detail_photo2');
+	            $full_site_photo = get_sub_field('full_site_photo');
+	            $key_features = get_sub_field('key_features'); ?>
+  		        <div class="details">
+  		        	<div class="left">
+                  <?php if ($detail_photo || $detail_photo2) { ?>
   		        		<div class="detail">
-  		        			<img src="<?php echo $detail_photo['url']; ?>">
+                    <?php if ($detail_photo) { ?>
+                    <figure><img src="<?php echo $detail_photo['url']; ?>" alt=""></figure>
+                    <?php } ?>
+  		        			<?php if ($detail_photo2) { ?>
+                    <figure><img src="<?php echo $detail_photo2['url']; ?>" alt=""></figure>
+                    <?php } ?>
   		        		</div>
+                  <?php } ?>
+
+                  <?php if ($key_features) { ?>
+  		        		<div class="key-features">
+  		        			<h2>Key Features</h2>
+  		        			<?php echo $key_features; ?>
+  		        		</div>
+                  <?php } ?>
+
+  		        	</div>
+                <?php if ($full_site_photo) { ?>
+  		        	<div class="right">
+  		        		<img src="<?php echo $full_site_photo['url']; ?>">
+  		        	</div>
                 <?php } ?>
-		        		<div class="key-features">
-		        			<h2>Key Features</h2>
-		        			<?php echo $key_features; ?>
-		        		</div>
-		        	</div>
-              <?php if ($full_site_photo) { ?>
-		        	<div class="right">
-		        		<img src="<?php echo $full_site_photo['url']; ?>">
-		        	</div>
-              <?php } ?>
-		        </div>
+  		        </div>
 
 		        <?php
 		        // Case: Download layout.
 		        elseif( get_row_layout() == 'color_palette' ): 
-		            $text_field = get_sub_field('text_field');
-		            $colorsText = get_sub_field('colors_description');
-                $colors = get_sub_field('colors');
-                $color_class = ($colorsText && $colors) ? 'half':'full';
-		        ?>
-		        <div class="colors">
-		        	<div class="left testimonial-info">
-                <div class="inner">
-                  <?php echo $text_field; ?>
-                </div>
-		        	</div>
-
-              <?php if ($colors || $colorsText) { ?>
-		        	<div class="right">
-                <div class="flexwrap <?php echo $color_class ?>">
-                <?php if ($colorsText) { ?>
-                  <div class="fxcol colorsText"><?php echo $colorsText ?></div>
-                <?php } ?>
-                <?php if ($colors) { ?>
-                  <div class="fxcol variations">
-  		        		<?php foreach( $colors as $c ) { ?>
-  		        			<div class="color" style="background-color: <?php echo $c['color']; ?>"></div>
-  		        		<?php } ?>
+	            $text_field = get_sub_field('text_field');
+	            $colorsText = get_sub_field('colors_description');
+              $colors = get_sub_field('colors');
+              $color_class = ($colorsText && $colors) ? 'half':'full';
+  		        if ($text_field || $colors || $colorsText) { ?>
+  		        <div class="colors">
+                <?php if ($text_field) { ?>
+  		        	<div class="left testimonial-info">
+                  <div class="inner">
+                    <?php echo $text_field; ?>
                   </div>
+  		        	</div>
                 <?php } ?>
-                </div>
-		        	</div>
+
+                <?php if ($colors || $colorsText) { ?>
+  		        	<div class="right">
+                  <div class="flexwrap <?php echo $color_class ?>">
+                  <?php if ($colorsText) { ?>
+                    <div class="fxcol colorsText"><?php echo $colorsText ?></div>
+                  <?php } ?>
+                  <?php if ($colors) { ?>
+                    <div class="fxcol variations">
+    		        		<?php foreach( $colors as $c ) { ?>
+    		        			<div class="color" style="background-color: <?php echo $c['color']; ?>"></div>
+    		        		<?php } ?>
+                    </div>
+                  <?php } ?>
+                  </div>
+  		        	</div>
+                <?php } ?>
+  		        </div>
               <?php } ?>
-		        </div>
 		            
-
-		            
-		        
-
 
 		        <?php
 				// Case: Download layout.
