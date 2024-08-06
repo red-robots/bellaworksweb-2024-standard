ab8ba0eaaac80528b6578801ce97620cb1303c89
Lisa <lisaqdebona@gmail.com>
2024-08-06 02:46:19 -0400

    Styled About Us page

--- page-about.php
+++ page-about.php
@@ -11,57 +11,103 @@ get_header(); ?>
 
 <div class="wp-site-blocks">
 	<main class="wp-block-group is-layout-flow wp-block-group-is-layout-flow">
-        <div class="pagewrapper">
+		<?php
+		while ( have_posts() ) : the_post(); ?>
+    <header class="entryHeader">
+      <div class="inner">
+        <h1><?php the_title() ?></h1>
+      </div>
+    </header>
+    <?php  
+    $row1_title = get_field('row1_title');
+    $row1_text = get_field('row1_text');
+    ?>
+    <section class="service intro-text-wrap">
+      <div class="section-two">
+        <div class="left">
+          <?php if ($row1_title) { ?>
+          <div class="title">
+            <h2 class="wp-block-heading has-text-align-right">
+              <?php echo $row1_title ?>
+            </h2>
+          </div>
+          <?php } ?>
+        </div>
+
+        <?php if ($row1_text) { ?>
+        <div class="right">
+          <?php echo $row1_text ?>
+        </div>
+        <?php } ?>
+      </div>
+
+      <?php  
+      $row2_text_left = get_field('row2_text_left');
+      $row2_text_right = get_field('row2_text_right');
+      ?>
+
+      <div class="section-four">
+        
+        <?php if ($row2_text_left) { ?>
+        <div class="left alt">
+          <?php echo $row2_text_left ?>
+        </div>
+        <?php } ?>
 
 
-			<?php
-			while ( have_posts() ) : the_post(); ?>
-				<section class="about">
-					<div class="left">
-						<figure>
-							<img src="<?php bloginfo('template_url'); ?>/images/triangles.svg">
-						</figure>
-						<h1><?php the_title(); ?></h1>
-					</div>
-					<div class="right">
-						<?php the_content(); ?>
-					</div>
+        <div class="right">
+          <div class="wrapper">
+            <div class="dots">
+              <div class="dot-one"></div>
+              <div class="dot-two"></div>
+              <div class="dot-three"></div>
+              <div class="dot-four"></div>
+            </div>
+
+            <?php if ($row2_text_right) { ?>
+            <div class="right-text">
+              <?php echo $row2_text_right ?>
+            </div>
+            <?php } ?>
+          </div>
+        </div>
+      </div>
+     </section>
+
+
+		
+
+				<div class="section-title">
+					<h2>OUR TEAM</h2>
+				</div>
+
+			<section class="services">
 				
-				</section>
+				<?php 
+					$bios = get_field('bios');
 
-					<div class="section-title">
-						<h2>OUR TEAM</h2>
-					</div>
+					foreach( $bios as $b ) {
+						$name = $b['name'];
+						$pic = $b['pic'];
+						$bio = $b['bio'];
 
-				<section class="services">
-					
-					<?php 
-						$bios = get_field('bios');
-
-						foreach( $bios as $b ) {
-							$name = $b['name'];
-							$pic = $b['pic'];
-							$bio = $b['bio'];
-
-					 ?>
-						<div class="cube">
-							<h3><?php echo $name; ?></h3>
-              <?php if( isset($pic['url']) && $pic['url'] ) { ?>
-							<div class="pic">
-								<img src="<?php echo $pic['url']; ?>">
-							</div>
-              <?php } ?>
-							<div class="desc">
-								<?php echo $bio; ?>
-							</div>
+				 ?>
+					<div class="cube">
+						<h3><?php echo $name; ?></h3>
+            <?php if( isset($pic['url']) && $pic['url'] ) { ?>
+						<div class="pic">
+							<img src="<?php echo $pic['url']; ?>">
 						</div>
-					<?php } ?>
-				</section>
-			<?php endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</main>
+            <?php } ?>
+						<div class="desc">
+							<?php echo $bio; ?>
+						</div>
+					</div>
+				<?php } ?>
+			</section>
+		<?php endwhile; // End of the loop.
+		?>
+  </main><!-- #main -->
+</div>
 <?php
 get_footer();
